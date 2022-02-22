8268c1a9c685d19070543d105ecc92ea77f36cbc
Ray Ogunjimi <81774312+rayogunjimi@users.noreply.github.com>
2022-02-21 20:25:04 -0500

    Add files via upload

---
+++ google_autocomplete_scraper.py
@@ -0,0 +1,14 @@
+import requests
+import json 
+
+searchword = "hello"
+
+URL="http://suggestqueries.google.com/complete/search?client=firefox&q=" + searchword
+#URL="http://suggestqueries.google.com/complete/search?client=chrome&q=" + searchword
+
+response = requests.get(URL)
+
+result = json.loads(response.content.decode('utf-8')) 
+
+for completion in result[1]:
+    print(completion)
